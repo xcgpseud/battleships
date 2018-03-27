58be971ae770a2d7daa1252ba9fc1ef227969352
xcgpseud <xcgpseud@github.com>
2018-03-27 21:25:06 +0100

    started

--- Battleships/MainWindow.xaml.cs
+++ Battleships/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Battleships.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace Battleships
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DisplayUi(BoardViewModel o)
+        {
+            
         }
     }
 }
---
+++ Battleships/ViewModels/BoardViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Battleships.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships.ViewModels
+{
+    public class BoardViewModel
+    {
+        private Board board = new Board();
+
+        public string BoardName
+        {
+            get
+            {
+                return board.Name;
+            }
+            set
+            {
+                board.Name = value;
+            }
+        }
+
+        public string LblNameColour
+        {
+            get
+            {
+                if(board.Name == "test1")
+                {
+                    return "Blue";
+                }
+                else
+                {
+                    return "Red";
+                }
+            }
+        }
+    }
+}
